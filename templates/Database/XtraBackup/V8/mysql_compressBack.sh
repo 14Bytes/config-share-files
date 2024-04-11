5b766f9018793c9ac7d817863a979fdf41115204
J1nH4ng <jinhang@chinacici.com>
2024-04-11 10:14:24 +0800

    feat(cors.conf): nginx 跨域请求配置文件编写
    Signed-off-by: J1nH4ng <jinhang@chinacici.com>

---
+++ templates/Database/XtraBackup/V8/mysql_compressBack.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# Script Name: mysql_compressBack.sh
+#
+# Author: J1n H4ng<jinhang@mail.14bytes.com>
+# Date: April 11, 2024
+#
+# Last Editor: J1n H4ng<jinhang@mail.14bytes.com>
+# Last Modified:  April 11, 2024
+#
+# Description: XtraBackup Mysql 数据库备份脚本
---
+++ templates/Jenkins/build-java-package.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -ilex
+#
+# Script Name:  build-java-package.shb
+#
+# Author: J1n H4ng<jinhang@mail.14bytes.com>
+# Date: April 09, 2024
+#
+# Last Editor: J1n H4ng<jinhang@mail.14bytes.com>
+# Last Modified:  April 09, 2024
+#
+# Description: Build Java Project Package 4 Jenkins
+
+SERVER="[target ip1], [target ip2], [target ip3]"
+DIR=${data:-/data}
