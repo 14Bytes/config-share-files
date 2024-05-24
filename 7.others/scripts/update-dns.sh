9dc9bb477340a69a7197e470fb0cdecd45af413b
J1nH4ng <jinhang@mail.14bytes.com>
2024-05-24 16:49:59 +0800

    :sparkles: feat(scripts): 更新 dnsmasq 脚本编写

---
+++ 7.others/scripts/update-dns.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# 自动更新 Dnsmasq 脚本
+# 版权 2024 J1nH4ng<jinhang@mail.14bytes.com>
+
+# Globals:
+# Arguments:
+#  None
+
+# 判断记录是否存在
+function judge_remain() {
+  :
+}
+
+# 解析域名 test.com，并写入到 com.test.conf 配置文件在
+function get_hostname() {
+  :
+}
+
+# 解析需要绑定的 ip
+function get_ip() {
+  :
+}
+
+# 检查绑定的域名状态
+function status_check() {
+  :
+}
+
+# 函数入口
+function main() {
+  :
+}
+
+main "$@"
