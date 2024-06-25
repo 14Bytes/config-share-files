abaadd662f863b27cbb1f7fe5962d65e2c2765d0
J1nH4ng <jinhang@mail.14bytes.com>
2024-06-25 10:30:21 +0800

    :sparkles: feat(file_sync.sh): nginx 配置文件同步脚本

---
+++ 2.server-web/nginx/scripts/file_sync.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# nginx 配置文件同步脚本
+# 版权 2024 J1nH4ng<jinhang@icloud.com>
+
+# Globals:
+# Arguments:
+#  None
+
+function check_md5() {
+  :
+}
+
+function rsync_file() {
+  :
+}
+
+function execute_script() {
+  :
+}
+
+function main() {
+  :
+}
+
+main "$@"
