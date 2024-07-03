01d28357410b03f7d46fa1f1496ea9e8b02493c5
J1nH4ng <j1nh4ng@icloud.com>
2024-07-03 20:38:05 +0800

    :sparkles: feat(dump_backup.sh): 将压缩文件功能写入 dump_backup.sh 文件内容，而不是拆分为独立的函数文件
    将压缩文件功能写入 dump_backup.sh 文件内容，而不是拆分为独立的函数文件，便于控制需要压缩的文件内容和数量
    
    BREAKING CHANGE: 删除了 zip_and_clean.sh 文件

--- 7.others/shutils/server/dump_backup.sh
+++ 7.others/shutils/server/dump_backup.sh
@@ -4,15 +4,12 @@
 # 版权 2024 J1nH4ng<jinhang@mail.14bytes.com>
 
 # Globals:
-#  DB_USR: 数据库用户
-#  DB_PASSWD: 数据库密码
-#  BACKUP_DIR: 备份文件夹
-#  ZIP_DIR: 压缩文件夹
-#  LOG_DIR: 日志文件夹
+#
 # Arguments:
 #  None
 
 source ../shared/current_date_dir_make.sh
+source ../core/inwaer_logs.sh
 
 # 所有数据库备份
 function backup_all_DB() {
@@ -28,7 +25,10 @@ function backup_all_DB() {
   BACKUP_DIR=$3
   CURRENT_DATE=$(date +"%Y%m%d%H%M%S")
 
+  echo_info "所有的数据库将会被备份"
+  echo_info_logs "$0" "所有数据库被备份"
   "${MYSQLDUMP_BIN}" -u"${DB_USER}" -p"${DB_PASSWD}" --all-databases > "${BACKUP_DIR}"/"${CURRENT_DATE}"/all_databases.sql
+  echo_info_logs "$0" "所有数据库备份成功至 ${BACKUP_DIR}/${CURRENT_DATE}/ 目录下"
 }
 
 # 备份单独数据库
@@ -45,9 +45,25 @@ function dump_backup_test() {
   :
 }
 
+function zip_dir() {
+  if tar -jcxf "${BACKUP_DIR}"/"${CURRENT_DATE}".tar.gz "${BACKUP_DIR}"/"${CURRENT_DATE}"/; then
+    echo_info "文件已成功压缩"
+    echo_info_logs "$0" "文件已经成功压缩"
+    echo_alert "${BACKUP_DIR}/${CURRENT_DATE} 目录将会被删除"
+    echo_alert_logs "$0" "${BACKUP_DIR}/${CURRENT_DATE} 目录将会被删除"
+    rm -rf "${BACKUP_DIR}"/"${CURRENT_DATE:?}"
+    echo_alert_logs "$0" "${BACKUP_DIR}/${CURRENT_DATE} 目录被删除"
+  else
+    echo_error_basic "文件压缩失败，请再次手动执行或联系管理员处理"
+    echo_error_logs "$0" "文件压缩失败"
+    exit 1
+  fi
+}
+
 function dump_backup_main() {
-  current_date_path_main "/data/backup"
-  backup_all_DB "/usr/local/mysql8.0/bin/mysqldump" "[password]" "/data/backup"
+  current_date_path_main "/data/backup/mysql"
+  backup_all_DB "/usr/local/mysql8.0/bin/mysqldump" "[password]" "/data/backup/mysql"
+  zip_dir
 }
 
 # dump_backup_test "$@"
