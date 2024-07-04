23ea932afd63007feef91af8dd94057d78028c63
J1nH4ng <jinhang@mail.14bytes.com>
2024-07-04 10:42:52 +0800

    :memo: feat(optional_registry.sh): 编写注册表文件

--- 7.others/shutils/optional_registry.sh
+++ 7.others/shutils/optional_registry.sh
@@ -1,13 +1,7 @@
 # optional_registry 记录了函数与文件的对应关系
 
-# inwaer_logs.sh 中的函数
-#   - function echo_info(): info 类信息输出格式化函数
-#   - function echo_alert(): alert 类信息输出格式化函数
-#   - function echo_error_basic(): error 类信息输出格式化基础函数
-#   - function inwaer_test(): 该文件本身的测试函数
+# cores 目录下的文件不依赖与任何文件
+# shared 目录下的文件依赖与 cores 目录下的部分文件
+# server 目录下的文件为与工作内容有关的文件，依赖与 cores 和 shared 目录下的部分文件
 
-# md5.sh 中的函数
-#   - function init_signal_md5(): 生成单文件的 md5
-#   - function init_dir_md5(): 为文件夹下是所有文件生成一个 md5
-#   - function check_md5(): 校验文件的 md5
-#   - function md5_test(): 该文件本身的测试函数
+
