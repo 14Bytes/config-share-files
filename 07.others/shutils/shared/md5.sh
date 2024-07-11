66539090f1a5c9cea610076ea7a0c0c1500b4c26
J1nH4ng <jinhang@mail.14bytes.com>
2024-07-11 14:26:45 +0800

    :tada: feat(rust-factory): rust 项目模板

--- 07.others/shutils/shared/md5.sh
+++ 07.others/shutils/shared/md5.sh
@@ -11,15 +11,15 @@ source ./../core/inwaer_logs.sh
 
 function init_signal_md5() {
   # 输入文件的绝对路径，例如：/tmp/test.sh
-  md5sum "$1" > "$1.md5"
+  md5sum "$1" >> "$1.md5"
 }
 
 function init_dir_md5() {
-  :
+  find "$1/*" -type f -print0 | xargs -0 md5sum >> "$1.md5"
 }
 
 function check_md5() {
-  :
+  md5sum -c md5.txt >> md5.check
 }
 
 function md5_test() {
